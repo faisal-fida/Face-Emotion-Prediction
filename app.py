699ce56d7cd59c8217128c7ed6eceafae7cc47dc
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2024-04-23 20:50:05 +0000

    base64

--- app.py
+++ app.py
@@ -1,29 +1,51 @@
 import os
 import cv2
+import time
+import base64
 import numpy as np
-from keras.models import load_model
-from keras.preprocessing import image
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+from keras.models import load_model  # noqa: E402
+from keras.preprocessing import image  # noqa: E402
+import tensorflow as tf  # noqa: F401, E402
+
+avg_time = []
 
 SIZE = 48
-MODEL_PATH = "model.h5"
-HAARCASCADE_PATH = "utils/face.xml"
+TEST_IMAGES = "utils/test_images"
 OBJECTS = ("angry", "disgust", "fear", "happy", "sad", "surprise", "neutral")
 
+face_cascade = cv2.CascadeClassifier("utils/face.xml")
+model = load_model("utils/model.h5")
 
-def detect_single_face(image_path):
-    img = cv2.imread(image_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    face_cascade = cv2.CascadeClassifier(HAARCASCADE_PATH)
+with open("cb-base64-string.txt", "r") as f:
+    base64_string = f.read()
+    sbuf = base64.decodebytes(base64_string.encode())
+    pimg = np.frombuffer(sbuf, dtype=np.uint8)
+    IMAGE = cv2.imdecode(pimg, cv2.IMREAD_UNCHANGED)
+
+
+def detect_single_face(image):
+    """
+    Detects a single face in an image and returns the face image
+    """
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(SIZE, SIZE))
     if len(faces) > 0:
         (x, y, w, h) = faces[0]
         face_image = gray[y : y + h, x : x + w]
         return face_image
     else:
-        raise Exception("No face detected, skipping prediction")
+        return None
 
 
 def preprocessing(face_image):
+    """
+    Preprocesses the face image
+    """
+
     x = cv2.resize(face_image, (SIZE, SIZE))
     x = x.reshape(x.shape + (1,))
     x = image.img_to_array(x)
@@ -31,26 +53,32 @@ def preprocessing(face_image):
     return x / 255
 
 
-def load_and_predict_model(image_path, model):
-    face_image = detect_single_face(image_path)
+def predict(model, face_image):
+    """
+    Load the model and predict emotions from the image
+    """
+
+    if face_image is None:
+        return "No face detected"
     preprocessed = preprocessing(face_image)
     predictions = model.predict(preprocessed)
-    predictions = np.argmax(predictions[0])
-    # Show image and prediction
-    cv2.imshow("Image", cv2.imread(image_path))
-    return OBJECTS[predictions]
+    predictions = OBJECTS[np.argmax(predictions)]
+    return predictions, avg_time
 
 
 def main():
-    model = load_model(MODEL_PATH)
-    for i in os.listdir("images"):
-        if i.endswith(".jpg"):
-            image_path = os.path.join("images", i)
-            prediction = load_and_predict_model(image_path, model)
-            print(f"Prediction for {i}: {prediction}")
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+    time_start = time.time()
+    face_image = detect_single_face(IMAGE)
+    prediction, avg_time = predict(model, face_image)
+    time_end = time.time()
+    avg_time.append(time_end - time_start)
+    print(f"Prediction: {prediction}, Time: {round(sum(avg_time) / len(avg_time), 4)}")
+
+    # Save the face image
+    # file_name = f"pred/{prediction}_{time.time()}.png"
+    # cv2.imwrite(file_name, face_image)
 
 
 if __name__ == "__main__":
-    main()
+    for _ in range(200):
+        main()
